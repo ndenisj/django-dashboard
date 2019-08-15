2fb4371eed93cdeba077327384f532b41199a79b
Denis John <denis.john.ndarake@gmail.com>
2019-08-15 10:40:53 +0100

    Add new finance app with Rest API Framework with ploty.js

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'notepad.apps.NotepadConfig',
     'news.apps.NewsConfig',
     'finance.apps.FinanceConfig',
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from finance.views import company_article_list, ChartData
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notes/', include('notepad.urls')),
+    path('companies/', company_article_list, name="companies"),
+    path('api/chart/data/', ChartData.as_view(), name="api-chart-data"),
 ]
 
 if settings.DEBUG:
